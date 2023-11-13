590c0f7d7c96fe012d0f4bd36e37cfe59f8b7cf4
unknown <imandev.989@gmail.com>
2023-11-13 18:34:19 +0330

    UPGRADE

--- app/[locale]/subpages/[slug]/page.jsx
+++ app/[locale]/subpages/[slug]/page.jsx
@@ -1,33 +1,31 @@
 // "use client";
 
+import { getMyData } from "./data";
 
-
-import data from "./data.json"
+// import data from "./data.js";
 
 const Subpage = () => {
-  
+  const data = getMyData();
   return (
     <div className="d-flex flex-column min-vh-100">
-      <div  className="container mx-auto my-5 flex-grow-1">
-      <div className="flex">
-        {(data.blogs.map((data) => (
-          <div  key={data.title} className="mb-3">
-            <div className="border-none">
-              <div>
-                <p>
-                <div dangerouslySetInnerHTML={{__html: data}}></div>
-
-                 </p>
-                <p>
-                <div dangerouslySetInnerHTML={{__html: data}}></div>
-
-                </p>
+      <div className="container mx-auto my-5 flex-grow-1">
+        <div className="flex">
+          {data.map((data) => (
+            <div key={data.title} className="mb-3">
+              <div className="border-none">
+                <div>
+                  <p>
+                    <div dangerouslySetInnerHTML={{ __html: data }}></div>
+                  </p>
+                  <p>
+                    <div dangerouslySetInnerHTML={{ __html: data }}></div>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        )))}
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
